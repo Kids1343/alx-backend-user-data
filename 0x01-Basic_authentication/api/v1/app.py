30b26c610570295d43724eac611ae02d6c7a73a0
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-05-19 09:16:03 +0300

    Update app.py

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,9 +16,19 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.errorhandler(404)
 def not_found(error) -> str:
-    """ Not found handler
-    """
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Error handler for 401, unauthorized"""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Error handler for 403 forbidden"""
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
